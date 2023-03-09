55e43db9e0872b6e373e868fe63ae495ff2b1fa6
Shubham Bhavin Dhakan <103954473+Shubham-Dhakan@users.noreply.github.com>
2023-03-09 13:16:13 -0500

    commit new changes

---
+++ portfolio-app/src/data/data.js
@@ -0,0 +1,35 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"RestaurantWebsite",
+        image:WorkIm,
+        github:"https://github.com/Shubham-Dhakan/RestaurantListingWebsite/tree/main/overreacted-zomato",
+        live:"https://lnkd.in/gVUDKru6",
+    },
+    {
+        id:2,
+        name:"Rental-Car Website",
+        image:RealEst,
+        github:"https://github.com/Shubham-Dhakan/RentalCars-Listing-Website/tree/main/car-listing-portal",
+        live:"https://rentalcar-listing-website.netlify.app/",
+    },
+    {
+        id:3,
+        name:"Cash-Register-Manager ",
+        image:WorkIm,
+        github:"https://github.com/Shubham-Dhakan/cash-register-vanillaJs",
+        live:"https://cash-register-website.netlify.app",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+
+
+]
